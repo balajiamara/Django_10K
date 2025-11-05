898cb680637207d1697add40389e21a48665c065
Balaji Amara <balajiamara2004@gmail.com>
2025-11-05 19:20:45 +0530

    Allowed hosts updated

--- Restaurant_CRUD/menu_pro/menu_pro/settings.py
+++ Restaurant_CRUD/menu_pro/menu_pro/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-3so=qxe_)fx69js5!o0%uw1by7rkus)2617l=p@@tzukonec=_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
 
 # Application definition
